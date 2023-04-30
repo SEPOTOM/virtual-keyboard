19aac8feb604f5ca63d2f64fb9d6d0e6a042edbe
Artyom Pizin <artempetr2016@gmail.com>
2023-04-30 16:24:38 +0300

    feat: add .page class and add it to body

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import KeyLabels from './js/key-labels/key-labels.js';
 
+const ClassNames = {
+  PAGE: 'page',
+};
+
 async function initApp() {
+  document.body.className = ClassNames.PAGE;
+
   await KeyLabels.fetch();
 }
 
